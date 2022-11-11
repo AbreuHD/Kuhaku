16e0aafbb768394b81d6bcba58058da7e1535d96
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2022-11-11 09:31:23 -0400

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< Updated upstream
   content: ["./src/**/*.{html,ts}",],
+=======
+  content: ["./src/**/*.{html,ts,css}"],
+>>>>>>> Stashed changes
   theme: {
     extend: {},
   },
