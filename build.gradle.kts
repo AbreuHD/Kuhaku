5c4895880bfad64afd2d0209e239692228884025
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2024-09-22 21:14:40 -0400

    Library Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@
 plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-
     id("com.google.dagger.hilt.android") version "2.49" apply false
 }
